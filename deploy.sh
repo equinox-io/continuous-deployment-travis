95e15c38bbbb1f2b43310f77bbf7cd5de5c680d4
Kyle Conroy <kyle@kyleconroy.com>
2016-06-12 17:43:38 -0700

    First pass at an example

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -eo pipefail
+
+# Download and unpack the most recent Equinox release tool
+wget https://bin.equinox.io/c/mBWdkfai63v/release-tool-stable-linux-amd64.tgz
+tar -vxf release-tool-stable-linux-amd64.tgz
+
+./equinox release \
+        --version="$TRAVIS_COMMIT" \
+        --app="app_4xCaMumNoeF" \
+        --platforms="darwin_amd64 linux_amd64 windows_amd64" \
+        --signing-key="equinox.key.enc" \
+        --token="$EQUINOX_API_TOKEN" \
+        .
